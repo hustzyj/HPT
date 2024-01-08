a30a13ab4e04f7c3b8fcee4636f0229d7803f301
zyj <zyj_hust@hust.edu.cn>
2024-01-08 22:30:59 +0800

    dsd

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def main(config):
             is_best = acc1 > max_accuracy
             max_accuracy = max(max_accuracy, acc1)
             logger.info(f'Max accuracy: {max_accuracy:.2f}%')
-        if dist.get_rank() == 0 and epoch >23 and (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
+        if dist.get_rank() == 0  and (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
             epoch_saving(config, epoch, model.module, max_accuracy, optimizer, lr_scheduler, logger, config.OUTPUT, is_best)
 
     config.defrost()
